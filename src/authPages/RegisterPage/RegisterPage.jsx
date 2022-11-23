40406e566fb706e8b59a6cb80ef8b376358940d3
Res0ution <maestro@hotger.com>
2022-11-23 20:31:37 +0300

    add api

--- src/authPages/RegisterPage/RegisterPage.jsx
+++ src/authPages/RegisterPage/RegisterPage.jsx
@@ -1,8 +1,44 @@
+import { Typography } from "@mui/material"
+import { useState, useEffect } from "react"
 import AuthBox from "../../shared/components/AuthBox"
+import { validateRegisterForm } from "../../shared/utils/validators"
+import RegisterPageFooter from "./RegisterPageFooter"
+import RegisterPageInputs from "./RegisterPageInputs"
 
 const RegisterPage = () => {
+  const [mail, setMail] = useState('')
+  const [password, setPassword] = useState('')
+  const [username, setUsername] = useState('')
+  const [isFormValid, setIsFormValid] = useState(false)
+
+  useEffect(() => {
+    setIsFormValid(validateRegisterForm({ mail, username, password }))
+  }, [mail, username, password, setIsFormValid])
+
+  const handleRegister = () => {
+
+  }
+
   return (
-    <AuthBox>RegisterPage</AuthBox>
+    <AuthBox>
+      <Typography variant="h5" sx={{ color: '#fff' }}>
+        Create an account
+      </Typography>
+
+      <RegisterPageInputs
+        mail={mail}
+        setMail={setMail}
+        username={username}
+        setUsername={setUsername}
+        password={password}
+        setPassword={setPassword}
+      />
+
+      <RegisterPageFooter
+        isFormValid={isFormValid} 
+        handleLogin={handleRegister} 
+      />
+    </AuthBox>
   )
 }
 
--- src/shared/utils/validators.js
+++ src/shared/utils/validators.js
@@ -5,6 +5,14 @@ export const validateLoginForm = ({ mail, password }) => {
   return isMailValid && isPasswordValid
 }
 
+export const validateRegisterForm = ({ mail, password, username }) => {
+  const isMailValid = validateMail(mail)
+  const isPasswordValid = validatePassword(password)
+  const isUsernameValid = validateUsername(username)
+
+  return isMailValid && isPasswordValid && isUsernameValid
+}
+
 const validatePassword = (password) => {
   return password.length > 6 && password.length < 12
 }
@@ -12,4 +20,8 @@ const validatePassword = (password) => {
 const validateMail = (mail) => {
   const emailPattern = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g
   return emailPattern.test(mail)
+}
+
+const validateUsername = (username) => {
+  return username.length > 2 && username.length < 12
 }
